2d7f92c9557bd9690a2357e953f0d11b12fd4022
yahya997 <yahya.1997914@gmail.com>
2020-04-17 15:04:59 +0300

    " "

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+/*0xfff5a623*/
+final themeColor = Color(0xff203152);
+final primaryColor = Color(0xfff5a623);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'const.dart';
+import 'login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Demo',
+      theme: ThemeData(
+        primaryColor: themeColor,
+      ),
+      home: LoginScreen(title: 'CHAT'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
